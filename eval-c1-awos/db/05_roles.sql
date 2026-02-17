4994a987b99f1b7d30f818b138e3e4d5a0dea79f
mili-emzz <m37009146@gmail.com>
2026-02-16 21:45:46 -0600

    seguridad ya chida profe ayuda

--- eval-c1-awos/db/05_roles.sql
+++ eval-c1-awos/db/05_roles.sql
@@ -3,7 +3,10 @@ CREATE USER app_user WITH PASSWORD 'secure_app_password_2024';
 REVOKE ALL ON SCHEMA public FROM app_user;
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM app_user;
 
+GRANT CONNECT ON DATABASE evalc1_db TO app_user;
+
 GRANT USAGE ON SCHEMA public TO app_user;
+
 GRANT SELECT ON vw_sales_daily TO app_user;
 GRANT SELECT ON vw_top_products_ranked TO app_user;
 GRANT SELECT ON vw_inventory_risk TO app_user;
